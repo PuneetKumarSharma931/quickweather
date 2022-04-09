2d0d74023724adc2857aa57fa2ab9cf8a7f6e33e
Puneet Kumar Sharma <puneetkumarsharma@Puneets-MacBook-Air.local>
2022-04-09 18:51:02 +0530

    Updated port in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 
 app.use('/static', express.static('static'));
 
